a7f2529fa6e1e0157f2308aef41b62428380de91
Gaurav Singh <gtx25801@gmail.com>
2022-12-23 18:31:14 +0530

    dom task3

---
+++ index.js
@@ -0,0 +1,9 @@
+let header = document.getElementById('main-header');
+header.style.border = "solid 3px #000000";  
+
+let titles = document.getElementsByClassName("title");
+console.log(titles);
+
+addItems = titles[0];
+addItems.style.fontWeight = "bolder";
+addItems.style.color = "green"; 
