4b652dfa6e0a10f4f801e662379326f069770f12
Gaurav Singh <gtx25801@gmail.com>
2022-12-24 18:17:12 +0530

    Updated list style

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ console.log(titles);
 addItems = titles[0];
 addItems.style.fontWeight = "bolder";
 addItems.style.color = "green"; 
+
+let items = document.getElementsByClassName("list-group-item");
+
+items[2].style.backgroundColor = "green";
+
+for (let i = 0; i < items.length; i++) {
+    items[i].style.fontWeight = "bold";
+    
+}
