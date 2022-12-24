fffa3a423b720d3203100be4b68bdc174134ecb8
Gaurav Singh <gtx25801@gmail.com>
2022-12-24 18:36:56 +0530

    task 5

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ addItems = titles[0];
 addItems.style.fontWeight = "bolder";
 addItems.style.color = "green"; 
 
-let items = document.getElementsByClassName("list-group-item");
+// let items = document.getElementsByClassName("list-group-item");
+
+// items[2].style.backgroundColor = "green";
+
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight = "bold";
+    
+// }
+
+
+let items = document.getElementsByTagName("li");
 
 items[2].style.backgroundColor = "green";
 
