d4c18ddbf43052c6d710619308d83182769d06fe
Gaurav Singh <gtx25801@gmail.com>
2022-12-24 18:48:33 +0530

    task 6.1

--- index.js
+++ index.js
@@ -18,11 +18,17 @@ addItems.style.color = "green";
 // }
 
 
-let items = document.getElementsByTagName("li");
+// let items = document.getElementsByTagName("li");
 
-items[2].style.backgroundColor = "green";
+// items[2].style.backgroundColor = "green";
 
-for (let i = 0; i < items.length; i++) {
-    items[i].style.fontWeight = "bold";
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight = "bold";
     
-}
+// }
+
+secondListItem = document.querySelector(".list-group-item:nth-child(2)");
+secondListItem.style.backgroundColor = "green";
+
+thirdListItem = document.querySelector(".list-group-item:nth-child(3)");
+thirdListItem.style.display = "none";
