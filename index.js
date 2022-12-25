6b26bf3225b7d48b3ad10be6b4b63a6c7a104fb7
Gaurav Singh <gtx25801@gmail.com>
2022-12-25 11:15:00 +0530

    task 9

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 let form  = document.querySelector("#addForm");
 let itemList = document.querySelector("#items");
+let filter = document.querySelector("#filter");
 
 
 //submit event for form
 form.addEventListener("submit",addItem);
 itemList.addEventListener("click",deleteItem);
+filter.addEventListener("keyup",filterItems);
 
 //addItem function
 function addItem(e){
     e.preventDefault();
 
     let newItemData = document.getElementById("item");
+    let newItemDescrip = document.getElementById("item-description");
     
     
     let li = document.createElement("l1");
     li.className = "list-group-item";
-    li.appendChild(document.createTextNode(newItemData.value))
+    li.appendChild(document.createTextNode(`${newItemData.value} | ${newItemDescrip.value}`));
     let btn = document.createElement("button");
     btn.className = "btn btn-danger btn-sm float-right delete";
     btn.appendChild(document.createTextNode("X"));
@@ -31,4 +34,18 @@ function deleteItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+function filterItems(e){
+    let text = e.target.value.toLowerCase();
+    let items = itemList.getElementsByTagName("li");
+    Array.from(items).forEach((item)=>{
+        let itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display="block";
+        }
+        else{
+            item.style.display="none";
+        }
+    });
 }
