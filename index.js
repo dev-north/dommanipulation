b107f4afe5f789fe465cb4d050e31a9a611bfa24
Gaurav Singh <gtx25801@gmail.com>
2022-12-25 10:27:15 +0530

    task 7

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let header = document.getElementById('main-header');
-header.style.border = "solid 3px #000000";  
+// let header = document.getElementById('main-header');
+// header.style.border = "solid 3px #000000";  
 
-let titles = document.getElementsByClassName("title");
-console.log(titles);
+// let titles = document.getElementsByClassName("title");
+// console.log(titles);
 
-addItems = titles[0];
-addItems.style.fontWeight = "bolder";
-addItems.style.color = "green"; 
+// addItems = titles[0];
+// addItems.style.fontWeight = "bolder";
+// addItems.style.color = "green"; 
 
 // let items = document.getElementsByClassName("list-group-item");
 
@@ -33,12 +33,66 @@ addItems.style.color = "green";
 // thirdListItem = document.querySelector(".list-group-item:nth-child(3)");
 // thirdListItem.style.display = "none";
 
-listItems = document.querySelectorAll(".list-group-item");
-console.log(listItems);
-listItems[1].style.color = "green";
+// listItems = document.querySelectorAll(".list-group-item");
+// console.log(listItems);
+// listItems[1].style.color = "green";
 
 
-oddListItems = document.querySelectorAll(".list-group-item:nth-child(odd)");
-oddListItems.forEach(oddListItem => {
-    oddListItem.style.backgroundColor = "green";
-});
+// oddListItems = document.querySelectorAll(".list-group-item:nth-child(odd)");
+// oddListItems.forEach(oddListItem => {
+//     oddListItem.style.backgroundColor = "green";
+// });
+
+//parent Element
+var ItemList = document.querySelector("#items");
+console.log(ItemList.parentElement);
+
+
+//children
+console.log(ItemList.children); //returns a html collection
+
+console.log(ItemList.childNodes); //returns a list node
+
+//accesing specific children
+console.log(ItemList.children[1]); //returns second child
+
+//accesing first child that is an element
+console.log(ItemList.firstElementChild); //returns first li
+
+console.log(ItemList.firstChild); //returns whitespace (text node)
+
+//accesing last child
+
+console.log(ItemList.lastChild); //returns text node 
+console.log(ItemList.lastElementChild); 
+
+//accesing siblings
+console.log(ItemList.nextSibling);
+console.log(ItemList.nextElementSibling); //in our case it doesnt have any 
+
+console.log(ItemList.previousSibling); //text node
+console.log(ItemList.previousElementSibling); //returns h2
+
+
+let textInput = document.querySelector("input[type = text]");
+textInput.setAttribute("placeholder", "Item Name");
+
+
+
+let container = document.querySelector("#main-header").firstElementChild;
+
+
+let helloWorld = document.createElement("p");
+let text  = document.createTextNode("Hello World");
+helloWorld.appendChild(text);
+
+
+let h1 = container.firstElementChild
+container.insertBefore(helloWorld , h1);
+
+let newElement = document.createElement("li");
+newElement.className = "list-group-item";
+let itemText = document.createTextNode("Hello World");
+newElement.appendChild(itemText);
+let firstItem = ItemList.firstElementChild
+ItemList.insertBefore(newElement,firstItem);
